6955c231513120a31367c9bf26e99a64836c7371
lobanov.office <bazzzzile@gmail.com>
2016-10-06 17:15:20 +0300

    первая рабочая версия скрипта. Пока без пакетной обработки

--- Helper/BKA/BKA_ql.py
+++ Helper/BKA/BKA_ql.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import shutil
 import xml.etree.ElementTree as ET
 # TODO разобраться с TEMPLATE (скорее всего, получится больше текста)
 from string import Template
+from PIL import Image
 
 src_file = r"C:\Users\lobanov\PycharmProjects\THE_BIG_ONE\Helper\BKA\QL\13_BKA15\Previews - from 20.05.2016\Previews - from 20.05.2016.kml"
-dst_dir = os.path.dirname(src_file)
 
+# TODO далее-временный блок, удалить, как только разберёшься выгрузке из сырого kmz
+temp_extracted_dir = os.path.dirname(src_file)
+
+dst_dir_name = 'QuickLooks'
+dst_dir_path = os.path.join(os.path.dirname(src_file), dst_dir_name)
+
+if not os.path.exists(dst_dir_path):
+    os.makedirs(dst_dir_path)
+
+# парсим kml
 tree = ET.parse(src_file)
 root = tree.getroot()
 
@@ -15,16 +26,16 @@ q = root.findall(".//GroundOverlay")
 
 # print(len(q))
 
-filename = q[0].find(".//href").text
-print(filename)
+ql_filename = q[0].find(".//href").text
+standard_ql_name = ql_filename[:13]
+ql_path = os.path.join(dst_dir_path, standard_ql_name + '.jpg')
+shutil.copy(os.path.join(temp_extracted_dir, ql_filename), ql_path)
+ql_image_obj = Image.open(ql_path)
+ql_width, ql_height = ql_image_obj.size[0], ql_image_obj.size[1]
 coords_str = q[0].find(".//coordinates").text
 # преобразуем строку с координатами углов в список и разбиваем по 4 точкам
 coords_lst = coords_str.split('\n')
-c1 = coords_lst[3]
-c2 = coords_lst[0]
-c3 = coords_lst[1]
-c4 = coords_lst[2]
-print c1, c2, c3, c4
+c1, c2, c3, c4 = coords_lst[3], coords_lst[0], coords_lst[1], coords_lst[2]
 
 text_content = Template("""
 !table
@@ -41,8 +52,8 @@ Definition Table
  CoordSys Earth Projection 1, 0
 """)
 text_content = text_content.substitute(
-    file_name=filename, map_coords1=c1, map_coords2=c2, map_coords3=c3, map_coords4=c4,
-    img_hight=str(518), img_width=str(516))
+    file_name=standard_ql_name + '.jpg', map_coords1=c1, map_coords2=c2, map_coords3=c3, map_coords4=c4,
+    img_hight=str(ql_height), img_width=str(ql_width))
 
-with open(os.path.join(dst_dir, filename.split('.')[0] + '.tab'), 'w') as f:
+with open(os.path.join(dst_dir_path, standard_ql_name + '.tab'), 'w') as f:
     f.write(text_content.strip())
