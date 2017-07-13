20d0f81f1f67cc3e7a6d56ae281f344879db5087
vasily_mac <lobanov.vk@gmail.com>
2017-07-13 12:02:36 +0300

    v1

---
+++ Misc/Downloaders/Sentinel/rename_Sentinel_tiles.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+# def s2_tile_rename(in_file):
+
+
+in_dir = r'/Users/vasily/!MyFiles/WORK/YANAO17/Sentinel2'
+for dirpath, dirnames, filenames in os.walk(in_dir):
+    for filename in filenames:
+        if filename == 'tileInfo.json':
+            # if filename.endswith('_preview.jp2'):
+            filepath = os.path.join(dirpath, filename)
+            # os.remove(filepath)
+            print('Processing ' + filepath)
+            id_0 = os.path.basename(os.path.dirname(filepath))
+            id_1 = os.path.basename(os.path.dirname(os.path.dirname(filepath)))
+            id = id_1.split('_')[-1] + '_' + id_0
+            shutil.copyfile(filepath, os.path.join(os.path.dirname(filepath), id + '_tileInfo.json'))
+
