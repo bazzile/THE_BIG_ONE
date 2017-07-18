697bc3b3c4c9acc0ed6174ec37431dd001bde1ca
vasily_mac <lobanov.vk@gmail.com>
2017-07-18 12:54:14 +0300

    new commit

--- Main/Downloaders/Sentinel/bands_merge.py
+++ Main/Downloaders/Sentinel/bands_merge.py
@@ -12,22 +12,36 @@ import sys
 import gdal_merge
 
 print('Model imported successfully!')
-user_input = raw_input("Enter the path to Sentinel-2 files root folder: ")\
+
+user_sat_input = raw_input("Select satellite: Sentinel-2 or Landsat-8. Type (S/L):")\
+    .decode(sys.stdin.encoding or locale.getpreferredencoding(True)).upper()
+if user_sat_input == 'S':
+    sat = 'Sentinel-2'
+elif user_sat_input == 'L':
+    sat = 'Landsat-8'
+else:
+    print('Received wrong input, terminating...')
+    exit()
+
+user_path_input = raw_input("Enter the path to {satellite} files root folder: ".format(satellite=sat))\
     .decode(sys.stdin.encoding or locale.getpreferredencoding(True))
 
-assert os.path.exists(user_input), "I did not find the file at, "+str(user_input)
-root_folder = user_input
+assert os.path.exists(user_path_input), "I did not find the file at, " + str(user_path_input)
+root_folder = user_path_input
 print('Path found, looking for Sentinel-2 files...')
 
 for dirpath, dirnames, filenames in os.walk(root_folder):
     for dirname in dirnames:
         dir_path = os.path.join(dirpath, dirname)
-        if any(re.match(r'.*B\d{2}\.jp2', f) for f in os.listdir(dir_path)):
-            id_0 = dirname
-            id_1 = os.path.basename(os.path.dirname(dir_path))
-            id = id_1.split('_')[-1] + '_' + id_0
-            out_file_name = id + '.tif'
-            print(dir_path, id, out_file_name)
-            bands = [os.path.join(dir_path, band + '.jp2') for band in ['B02', 'B03', 'B04']]
-            print('Merging ' + dir_path)
-            gdal_merge.main(['', '-separate', '-o', os.path.join(dir_path, out_file_name), bands[-1], bands[-2], bands[-3]])
+        if sat == 'Sentinel-2':
+            if any(re.match(r'.*B\d{2}\.jp2', f) for f in os.listdir(dir_path)):
+                id_0 = dirname
+                id_1 = os.path.basename(os.path.dirname(dir_path))
+                id = id_1.split('_')[-1] + '_' + id_0
+                out_file_name = id + '.tif'
+                print(dir_path, id, out_file_name)
+                bands = [os.path.join(dir_path, band + '.jp2') for band in ['B02', 'B03', 'B04']]
+                print('Merging ' + dir_path)
+                gdal_merge.main(['', '-separate', '-o', os.path.join(dir_path, out_file_name), bands[-1], bands[-2], bands[-3]])
+        elif sat == 'Landsat-8':
+            if any(re.match(r'LC8.*B\d{1,2}\.TIF', f) for f in os.listdir(dir_path)):
