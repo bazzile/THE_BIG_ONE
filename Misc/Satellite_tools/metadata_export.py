a631b6431385b81004b4e9d01cdf75f185f0ddad
lobanov.office <bazzzzile@gmail.com>
2017-07-20 18:47:27 +0300

    v1

--- Misc/Satellite_tools/metadata_export.py
+++ Misc/Satellite_tools/metadata_export.py
@@ -2,8 +2,15 @@ import os
 import re
 
 root_folder = os.path.dirname(os.path.abspath(__file__))
-
+scene_list = []
 for rootdir, dirnames, filenames in os.walk(root_folder):
     for filename in filenames:
         if re.match(r'.*_MTL\.txt', filename, re.IGNORECASE) is not None:
-            print(filename)
+            with open(os.path.join(rootdir, filename), 'r') as f:
+                d = {}
+                for line in f.read().splitlines():
+                    for parameter in ['LANDSAT_PRODUCT_ID', 'DATE_ACQUIRED', 'SPACECRAFT_ID',
+                                      'CLOUD_COVER_LAND', 'GRID_CELL_SIZE_PANCHROMATIC', 'GRID_CELL_SIZE_REFLECTIVE']:
+                        if parameter in line:
+                            d[parameter] = line.split('=')[1].strip()
+                print(d)
