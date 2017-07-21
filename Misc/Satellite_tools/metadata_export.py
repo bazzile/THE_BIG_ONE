6e2419d95baed116e6b0fdcb31cfe4ba022087c3
vasily_mac <lobanov.vk@gmail.com>
2017-07-21 11:38:45 +0300

    stable_version

--- Misc/Satellite_tools/metadata_export.py
+++ Misc/Satellite_tools/metadata_export.py
@@ -2,6 +2,11 @@ import os
 import re
 
 root_folder = os.path.dirname(os.path.abspath(__file__))
+out_dir = root_folder
+
+parameter_list = ['LANDSAT_PRODUCT_ID', 'DATE_ACQUIRED', 'SPACECRAFT_ID',
+                  'CLOUD_COVER_LAND', 'GRID_CELL_SIZE_PANCHROMATIC', 'GRID_CELL_SIZE_REFLECTIVE']
+
 scene_list = []
 for rootdir, dirnames, filenames in os.walk(root_folder):
     for filename in filenames:
@@ -9,8 +14,18 @@ for rootdir, dirnames, filenames in os.walk(root_folder):
             with open(os.path.join(rootdir, filename), 'r') as f:
                 d = {}
                 for line in f.read().splitlines():
-                    for parameter in ['LANDSAT_PRODUCT_ID', 'DATE_ACQUIRED', 'SPACECRAFT_ID',
-                                      'CLOUD_COVER_LAND', 'GRID_CELL_SIZE_PANCHROMATIC', 'GRID_CELL_SIZE_REFLECTIVE']:
+                    for parameter in parameter_list:
                         if parameter in line:
                             d[parameter] = line.split('=')[1].strip()
-                print(d)
+            scene_list.append(d)
+# sorting list of dictionaries by LANDSAT_PRODUCT_ID key
+sorted_scene_list = sorted(scene_list, key=lambda k: k['LANDSAT_PRODUCT_ID'])
+with open(os.path.join(out_dir, 'scene_info.txt'), 'w') as of:
+    csv_list = []
+    header = parameter_list
+    of.write(', '.join(parameter_list) + '\n')
+    for scene in sorted_scene_list:
+        of.write(', '.join(
+            [scene[parameter] for parameter in
+             parameter_list]) + '\n')
+
