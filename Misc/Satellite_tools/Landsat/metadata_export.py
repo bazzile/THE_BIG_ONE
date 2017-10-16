d177e2d3b7772985f1a445e9549dfb2c6d54425f
vasily_mac <lobanov.vk@gmail.com>
2017-10-17 01:18:22 +0300

    added S2 prod support

--- Misc/Satellite_tools/Landsat/metadata_export.py
+++ Misc/Satellite_tools/Landsat/metadata_export.py
@@ -1,8 +1,9 @@
 import os
 import re
+import json
 
 # root_folder = os.path.dirname(os.path.abspath(__file__))
-root_folder = r'U:\ОТА\ЯНАО17\Data\Imagery\Sentinel\подбор для мозаики\Тазовский'
+root_folder = r'/Users/vasily/!MyFiles/Coding/PycharmProjects/THE_BIG_ONE/Misc/Satellite_tools'
 out_dir = root_folder
 
 # product = input('Please choose product type: \nLandsat-8 C1 [L]\nSentinel-2 L1C from AWS [S]\nYour choice: ').upper()
@@ -15,10 +16,11 @@ if product == 'L':
                       'CLOUD_COVER_LAND', 'GRID_CELL_SIZE_PANCHROMATIC', 'GRID_CELL_SIZE_REFLECTIVE']
     sorting_parameter = 'DATE_ACQUIRED'
 elif product == 'S':
-    regex = r'tileInfo.json'
+    regex = r'productInfo.json'
     separator = ' : '
-    parameter_list = ["productName", "timestamp", "cloudyPixelPercentage", "utmZone", "latitudeBand", "gridSquare"]
-    sorting_parameter = "timestamp"
+    # parameter_list_tl = ["productName", "timestamp", "cloudyPixelPercentage", "utmZone", "latitudeBand", "gridSquare"]
+    parameter_list = ["product_id", "date_acquired"]
+    sorting_parameter = "date_acquired"
 else:
     raise Exception('Wrong parameter')
 
@@ -28,10 +30,15 @@ for rootdir, dirnames, filenames in os.walk(root_folder):
         if re.match(regex, filename, re.IGNORECASE) is not None:
             with open(os.path.join(rootdir, filename), 'r') as f:
                 d = {}
-                for line in f.read().splitlines():
-                    for parameter in parameter_list:
-                        if parameter in line:
-                            d[parameter] = line.split(separator)[1].strip().replace('"', '').replace(',', '')
+                if product == 'S':
+                    product_json = json.load(f)
+                    d['product_id'] = product_json['name'][:37]
+                    d['date_acquired'] = product_json["timestamp"][:10]
+                else:
+                    for line in f.read().splitlines():
+                        for parameter in parameter_list:
+                            if parameter in line:
+                                d[parameter] = line.split(separator)[1].strip().replace('"', '').replace(',', '')
             scene_list.append(d)
 # sorting list of dictionaries by sorting_parameter (i.e. LANDSAT_PRODUCT_ID) key
 sorted_scene_list = sorted(scene_list, key=lambda k: k[sorting_parameter], reverse=True)
