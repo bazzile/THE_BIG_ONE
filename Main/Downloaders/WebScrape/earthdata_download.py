c4fae5d6f983ad708a082a1bcdd50905a0c8531f
lobanov_WS008 <lobanov@innoter.com>
2018-01-12 15:29:10 +0300

    improved code

--- Main/Downloaders/WebScrape/earthdata_download.py
+++ Main/Downloaders/WebScrape/earthdata_download.py
@@ -5,7 +5,6 @@ import urllib.parse
 import bs4
 import re
 
-
 dst_dir = r'V:\POKROV\04_ASTER\BASIC\4'
 link = 'https://e4ftl01.cr.usgs.gov/PullDir/030429753818118/'
 username = "lobanov"
@@ -20,37 +19,22 @@ class SessionWithHeaderRedirection(requests.Session):
     AUTH_HOST = 'urs.earthdata.nasa.gov'
 
     def __init__(self, username, password):
-
         super().__init__()
-
         self.auth = (username, password)
-
         # Overrides from the library to keep headers when redirected to or from
-
         # the NASA auth host.
 
     def rebuild_auth(self, prepared_request, response):
-
         headers = prepared_request.headers
-
         url = prepared_request.url
-
         if 'Authorization' in headers:
-
             original_parsed = requests.utils.urlparse(response.request.url)
-
             redirect_parsed = requests.utils.urlparse(url)
-
-            if (original_parsed.hostname != redirect_parsed.hostname) and \
- \
-                            redirect_parsed.hostname != self.AUTH_HOST and \
- \
-                            original_parsed.hostname != self.AUTH_HOST:
-
+            if (original_parsed.hostname != redirect_parsed.hostname) \
+                    and redirect_parsed.hostname != self.AUTH_HOST \
+                    and original_parsed.hostname != self.AUTH_HOST:
                 del headers['Authorization']
-
         return
-
 
 # create session with the user credentials that will be used to authenticate access to the data
 session = SessionWithHeaderRedirection(username, password)
